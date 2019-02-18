17826ffb024685b37a417f56eba5c4b61a038a85
linbin <289873007@qq.com>
2019-02-18 11:24:19 +0800

    add store 2

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import 'antd/dist/antd.css';
-import { Input } from 'antd';
+import { Input, Button, List } from 'antd';
 import store from './store';
 
 class TodoList extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = store.getState();
-    console.log(this.state)
+    store.subscribe(this.handleSubscribe);
   }
   render() {
     return (
-      <Input />
+      <Fragment>
+        <Input value={this.state.inputValue} onChange={this.handleOnChange} placeholder="输入一些什么" style={{ width: 300 }} />
+        <Button type="primary" onClick={this.handleClick}>添加</Button>
+        <List
+          dataSource={this.state.list}
+          renderItem={item => (<List.Item>{item}</List.Item>)}
+        />
+      </Fragment>
     )
+  }
+  handleOnChange = (e) => {
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
+    console.log(e.target.value)
+    store.dispatch(action)
+  }
+  handleClick = () => {
+    const action = {
+      type: 'add_list',
+    }
+    store.dispatch(action)
+  }
+  handleSubscribe = () => {
+    this.setState(store.getState())
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,21 @@
 const defaultStore = {
-  inputValue: '123',
-  list: [1,2,3,4]
+  inputValue: '',
+  list: []
 }
 
 export default (state = defaultStore, action) => {
+  // console.log('reducer::', state, action)
+  let newValue;
+  switch(action.type){
+    case 'change_input_value':
+      newValue = Object.assign({}, state);
+      newValue.inputValue = action.value;
+      return newValue
+    case 'add_list':
+      newValue = Object.assign({}, state);
+      newValue.list.push(state.inputValue);
+      newValue.inputValue = '';
+      return newValue
+  }
   return state
 }
